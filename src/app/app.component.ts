83130929a96131de4ced1ddf29601b1fe9039c36
seraphium <viperking@163.com>
2017-04-25 13:49:48 +0800

    tried component instance control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { CountDownTimerComponent} from './shared/index';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
 })
-export class AppComponent  {
+export class AppComponent implements AfterViewInit{
+
+  @ViewChild(CountDownTimerComponent)
+  private timerComponent: CountDownTimerComponent;
+
+  seconds() {
+    return 0;
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => this.seconds = () => this.timerComponent.seconds, 0);
+
+  }
+
+  start() {
+    this.timerComponent.start();
+  }
+
+  stop() {
+    this.timerComponent.stop();
+  }
+
+
   constructor() {}
   name: string = 'Angular';
 
