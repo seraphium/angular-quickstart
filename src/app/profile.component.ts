e4e8b9bc8c3fe33086a6ca3a2455fc5f0bf1d9a3
seraphium <viperking@163.com>
2017-04-26 11:22:57 +0800

    tried RxJS observable

--- src/app/profile.component.ts
+++ src/app/profile.component.ts
@@ -2,14 +2,24 @@
  * Created by zezhang on 2017/4/25.
  */
 import { Component} from '@angular/core';
-
+import {UserService} from "./shared/user.service";
 
 @Component({
   selector: 'profile-page',
-  template: `<div>this is Eric's profile</div>`
+  template: `<div><button (click)="loadUser()">Load Profile</button>
+  {{ profile | json }}
+  </div>`
 })
 
 export class ProfileComponent {
-  constructor(){}
+  constructor(
+    private userService: UserService
+  ){}
+
+  profile = {};
+
+  loadUser()  {
+      this.userService.getUser().subscribe(data => this.profile = data);
+  }
 
 }
--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -2,17 +2,32 @@
  * Created by jackiezhang on 2017/4/25.
  */
 import { Injectable } from '@angular/core';
+import { Http, Response }from '@angular/http';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(
+    private http: Http
+  ) {
+    this.currentUserSubject.subscribe((val) => {
+      console.log(val);
+    });
 
-  currentUser = 'Eric';
-
-  setUser(name: string) {
-    this.currentUser = name;
   }
 
+  private currentUserSubject = new BehaviorSubject<string>('eric');
+  public currentUser = this.currentUserSubject.asObservable();
+
+  setUser(name: string) {
+    this.currentUserSubject.next(name);
+  }
+
+  getUser(){
+    return this.http.get(`https://conduit.productionready.io/api/profiles/eric`)
+      .map((res: Response) => res.json())
+  }
 }
 
 
